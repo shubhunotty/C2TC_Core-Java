823dae11408b135603752a0110590acf4148046e
shubh <shubh@MSI>
2022-02-22 13:30:12 +0530

    first commit

---
+++ MyClass/src/MyClass.java
@@ -0,0 +1,16 @@
+public class MyClass {
+
+	public static void main(String[] args) {
+		try {
+			int i;
+			return;
+		}
+	catch(Exception e) {
+		System.out.println("catchBlock");
+		
+	}
+		finally {
+			System.out.println("FinallyBlock");
+		}
+	}
+}
